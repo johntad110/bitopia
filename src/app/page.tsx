b393588ef37fc8c6809603ea860e0193c7a5b281
John Tad <johntad110@gmail.com>
2024-07-11 18:37:12 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import { WithTelegramProvider } from "@/components/webapp";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <WithTelegramProvider />
+  );
+}
---
+++ src/hooks/TelegramProvider.tsx
@@ -0,0 +1,48 @@
+import Script from "next/script";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import type { ITelegramUser, IWebApp } from "@/types";
+
+export interface ITelegramContext {
+    webApp?: IWebApp;
+    user?: ITelegramUser;
+}
+
+export const TelegramContext = createContext<ITelegramContext>({});
+
+export const TelegramProvider = ({
+    children,
+}: {
+    children: React.ReactNode;
+}) => {
+    const [webApp, setWebApp] = useState<IWebApp | null>(null)
+
+    useEffect(() => {
+        const app = (window as any).Telegram?.WebApp;
+        if (app) {
+            app.ready();
+            setWebApp(app);
+        }
+    }, [])
+
+    const value = useMemo(() => {
+        return webApp
+            ? {
+                webApp,
+                unsafeData: webApp.initDataUnsafe,
+                user: webApp.initDataUnsafe.user,
+            }
+            : {};
+    }, [webApp]);
+
+    return (
+        <TelegramContext.Provider value={value}>
+            <Script
+                src="https://telegram.org/js/telegram-web-app.js"
+                strategy="beforeInteractive"
+            />
+            {children}
+        </TelegramContext.Provider>
+    );
+};
+
+export const useTelegram = () => useContext(TelegramContext);
