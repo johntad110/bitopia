6b8c2d4283b47e95cf7a2456e301ce90ff8ed356
John Tad <johntad110@gmail.com>
2024-07-17 11:23:20 +0300

    update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Sora } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Sora({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bitopia - Tap into the Future of Bitcoin",
